93dfe81c705a057977e8b73c37dd8a7b1e61ca8a
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-12-10 13:21:35 +0100

    Agregar archivos de proyecto.

---
+++ ChatBox/CustomCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatBox
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Salir = new RoutedUICommand
+        (
+            "Salir",
+            "Salir",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.S, ModifierKeys.Control)
+            }
+        );
+    }
+}
---
+++ ChatBox/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatBox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            listaPrincipal = new ListBox();
+            InitializeComponent();
+        }
+
+        private void NewCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            listaPrincipal.Items.Clear();
+        }
+
+        private void NewCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (listaPrincipal.Items.Count > 0)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+
+        private void SalirCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SalirCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
